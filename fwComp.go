3098ec29cb7cd97abcab12c4b6343dd8cff539b7
kawayoshikun <elysionpi@gmail.com>
2020-10-25 11:42:15 +0900

    ソース追加

---
+++ fwComp.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/smtp"
+	"net/url"
+	"os"
+	"strconv"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/bitly/go-simplejson"
+)
+
+//出力ファイルパス
+const rsFilePath string = "./outFile/"
+
+//フォロワーのUSERIDを出力するファイル名
+const oldFollowerFile string = "old.txt"
+
+//処理結果ファイル名
+const rsFile string = "result.txt"
+
+func main() {
+
+	var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
+	fmt.Println("**********処理開始**********")
+	fmt.Println("アカウントIDを入力してください: ")
+	scanner.Scan()
+	account := scanner.Text()
+	if account == "gon_gonk" || account == "blackwarabi" {
+
+		rsList, e := readOldFollower(rsFilePath, oldFollowerFile, account)
+		if err := e; err != nil {
+			panic(err)
+		}
+
+		if err := followersComparison(rsList, rsFilePath, rsFile, account); err != nil {
+			panic(err)
+		}
+
+		if err := outputFollower(rsFilePath, oldFollowerFile, account); err != nil {
+			panic(err)
+		}
+	} else {
+		fmt.Println("対象アカウント以外のアカウントが指定されました。")
+	}
+	fmt.Println("**********処理完了**********")
+	fmt.Scanf("h")
+}
+
+/*
+TwitterAPIを呼び出すのに必要なトークン等を設定する
+*/
+func setTwKey(account string) *anaconda.TwitterApi {
+	//TwitterのAPIトークン
+	//jsonファイルの読み込み
+	bytes, err := ioutil.ReadFile("./context.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// []byte型からjson型へ変換
+	json, _ := simplejson.NewJson(bytes)
+	//アカウントで使用トークンを分ける
+	var twikey = map[string]string{}
+	if account == "blackwarabi" {
+		twikey = map[string]string{
+			"cons_key":  json.Get("blackwarabi_cons_key").MustString(),
+			"cons_sec":  json.Get("blackwarabi_cons_sec").MustString(),
+			"accto_key": json.Get("blackwarabi_accto_key").MustString(),
+			"accto_sec": json.Get("blackwarabi_accto_sec").MustString(),
+		}
+	} else if account == "gon_gonk" {
+		twikey = map[string]string{
+			"cons_key":  json.Get("gonk_cons_key").MustString(),
+			"cons_sec":  json.Get("gonk_cons_sec").MustString(),
+			"accto_key": json.Get("gonk_accto_key").MustString(),
+			"accto_sec": json.Get("gonk_accto_sec").MustString(),
+		}
+	}
+
+	anaconda.SetConsumerKey(twikey["cons_key"])
+	anaconda.SetConsumerSecret(twikey["cons_sec"])
+	api := anaconda.NewTwitterApi(twikey["accto_key"], twikey["accto_sec"])
+	return api
+}
+
+/*
+old.txtより前回実行時のフォロワーのUSERIDを取得し、スライスに格納して呼び出し元に返す
+*/
+func readOldFollower(filepath string, filename string, account string) (reList []string, err error) {
+	//txtファイルより前回のフォロワー一覧を取得する
+	file, err := os.OpenFile(filepath+account+"_"+filename, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(file)
+	//戻り値用のスライスを宣言
+	var rsSlice []string
+	//読み取ったold.txtのUSERIDをスライスに格納
+	for scanner.Scan() {
+		scText := scanner.Text()
+		rsSlice = append(rsSlice, scText)
+	}
+	defer file.Close()
+	return rsSlice, nil
+}
+
+/*
+過去のフォロワーのUSERIDをold.txtファイルから取得し、現在のフォロワーのUSERIDと比較する。
+その後、差分が出たUSERIDをresult.txtに出力する
+*/
+func followersComparison(list []string, filepath string, filename string, account string) error {
+	api := setTwKey(account)
+	pages := api.GetFollowersIdsAll(nil)
+	var rsSlice []string
+	for page := range pages {
+		for j := 0; len(page.Ids) > j; j++ {
+			toString := strconv.Itoa(int(page.Ids[j]))
+			rsSlice = append(rsSlice, toString)
+		}
+	}
+
+	//処理結果用txtファイル作成
+	file, err := os.Create(filepath + account + "_" + filename)
+	if err != nil {
+		return err
+	}
+
+	//比較
+	//双方の要素数が前回取得<=今回取得場合は処理しない
+	if len(list) <= len(rsSlice) {
+		fmt.Println("処理対象なし")
+		return nil
+	}
+
+	for i := 0; len(list) > i; i++ {
+		//リムーブされたフォロワーを検索し、USERIDをファイルに出力
+		if !arrayContains(rsSlice, list[i]) {
+			toInt, _ := strconv.Atoi(list[i])
+			v := url.Values{}
+			//USERIDからスクリーンネームとアカウント名を取得
+			userdata, _ := api.GetUsersShowById(int64(toInt), v)
+			_, err := file.WriteString("ID:" + userdata.ScreenName + " アカウント名:" + userdata.Name + "\n")
+			if err != nil {
+				return err
+			}
+			fmt.Println("ID:" + userdata.ScreenName + " アカウント名:" + userdata.Name)
+		}
+	}
+	//処理結果をGMAILで送信
+	if err := sendGmail(account); err != nil {
+		return err
+	}
+	defer file.Close()
+	return nil
+}
+
+/*
+現在のフォロワーのUSERIDを取得し、結果をold.txtに出力する
+*/
+func outputFollower(filepath string, filename string, account string) error {
+	file, err := os.Create(filepath + account + "_" + filename)
+	if err != nil {
+		return err
+	}
+	api := setTwKey(account)
+	pages := api.GetFollowersIdsAll(nil)
+	for page := range pages {
+		for cnt := 0; len(page.Ids) > cnt; cnt++ {
+			toString := strconv.Itoa(int(page.Ids[cnt]))
+			_, err := file.WriteString(toString + "\n")
+			if err != nil {
+				return err
+			}
+		}
+	}
+	defer file.Close()
+	return nil
+}
+
+/*
+配列の中に特定のUSERIDが含まれるか探索
+*/
+func arrayContains(arr []string, str string) bool {
+	for _, v := range arr {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+result.txtを元にメール送信
+*/
+func sendGmail(account string) error {
+	//jsonファイルの読み込み
+	bytes, err := ioutil.ReadFile("./context.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// []byte型からjson型へ変換
+	json, _ := simplejson.NewJson(bytes)
+	auth := smtp.PlainAuth(
+		"",
+		json.Get("address").MustString(),
+		json.Get("passwd").MustString(),
+		"smtp.gmail.com",
+	)
+
+	//txtファイルより処理結果一覧を取得する
+	file, err := os.OpenFile(rsFilePath+account+"_"+rsFile, os.O_RDONLY, 0666)
+	if err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(file)
+	//メール本文用の文字列を作成
+	var message string
+	//読み取った内容を連結文字列にする
+	for scanner.Scan() {
+		message += scanner.Text() + "\r\n"
+	}
+
+	err2 := smtp.SendMail(
+		"smtp.gmail.com:587",
+		auth,
+		json.Get("address").MustString(),
+		[]string{json.Get("address").MustString()},
+		[]byte(
+			"To:\r\n"+
+				"Subject:リムーブアカウント\r\n"+
+				"\r\n"+
+				message),
+	)
+	if err2 != nil {
+		return err2
+	}
+	defer file.Close()
+	return nil
+}
